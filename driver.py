336e260ced7678fc62b4dcefcba9c56750d6b149
seanperry <seanep94@gmail.com>
2024-03-30 14:56:42 -0700

    itemInDatabase working

--- driver.py
+++ driver.py
@@ -2,3 +2,5 @@ import osrsWikiAPI as osrs
 
 # osrs.testLaunch()
 osrs.itemDatabase()
+airRuneId = osrs.itemInDatabase('Air Rune')
+print(airRuneId)
--- osrsWikiAPI/__init__.py
+++ osrsWikiAPI/__init__.py
@@ -4,6 +4,10 @@ import os
 
 CURR_DIR = os.path.abspath(__file__).replace('__init__.py', '')
 
+ItemsTable = None
+GLOBAL_DF = None # This will be used for now, will replace with SQL in Django
+
+
 HEADERS = {
     'user-agent' : "Monster Drop Rates(Resume Project)", # OSRS Wiki requests you set this so they know why you're scrapping. 
 }
@@ -17,13 +21,18 @@ def itemDatabase():
     ### Until Django is implemented this will feed into a csv. ###
       
     '''
+
+
+    ''' Uncomment this when you want to work on the API call again. Stop pinging them lol
+    
     url = 'https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson'
     out = requests.get(url, headers=HEADERS)
     
     newItemsTable = pd.DataFrame.from_dict(out.json(), orient='index') \
                                 .reset_index() \
                                 .rename(columns={'index':'item', 0:'api_id'}) \
-
+    
+    newItemsTable['check item'] = newItemsTable['item'].str.lower()
     # Remove the LAST_UPDATE and LAST_UPDATE_F
     newItemsTable.drop(index=[0,1],inplace=True)
 
@@ -34,13 +43,16 @@ def itemDatabase():
 
     # This part will be replaced by reading in SQL not CSV
     if (not os.path.exists(CURR_DIR + 'db/table.csv')):
-        print('here')
         newItemsTable.to_csv(CURR_DIR + 'db/table.csv', index=False)
         itemsTable = newItemsTable.copy()
     else:
-        print('found')
         itemsTable = pd.read_csv(CURR_DIR + 'db/table.csv')
     newItemsTable = newItemsTable.copy()
+    '''
+    # Temp code, so I stop calling API on every run.. ###
+    itemsTable = pd.read_csv(CURR_DIR + 'db/table.csv') #
+    newItemsTable = itemsTable.copy()                   #
+    #####################################################
 
     # Join to find what needs to be inserted.
     df = pd.merge(
@@ -60,12 +72,18 @@ def itemDatabase():
     #     ItemsTable(item=item, api_id=api_id).save()
     
     # Pandas Version...
-    pd.concat([itemsTable, insertQuery]).to_csv(CURR_DIR + 'db/table.csv', index=False)
+    global GLOBAL_DF
+    GLOBAL_DF = pd.concat([itemsTable, insertQuery])
+    GLOBAL_DF.to_csv(CURR_DIR + 'db/table.csv', index=False)
 
-
-def testLaunch():
-    testUrl = f'prices.runescape.wiki/api/v1/osrs'
-
-    out = requests.get(testUrl, headers=HEADERS)
-    print(out.status_code)
-    print(out.json())
+def itemInDatabase(itemName:str):
+    '''
+    Function returns either True and the U-ID of the item or -1 if not found.
+    '''
+    #item = ItemsTable.objects.filter(item=itemName)
+    # if (len(queryset)): ....
+    global GLOBAL_DF
+    val  = GLOBAL_DF[(GLOBAL_DF['check item'] == itemName.lower())]
+    if (len(val)):
+        return val.values[0][1]
+    return -1
